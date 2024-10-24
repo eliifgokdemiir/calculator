687a3e592b9efbae451b1c5d582f3f4ddcb29a53
eliifgokdemiir <gokdemirelif12@gmail.com>
2024-10-24 13:09:03 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Elif Gökdemir on 23.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if let secondNumber = Int(secondNumber.text!){
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+        
+        
+      
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if let secondNumber = Int(secondNumber.text!){
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if let secondNumber = Int(secondNumber.text!){
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if let secondNumber = Int(secondNumber.text!){
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+}
+
